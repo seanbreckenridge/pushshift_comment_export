364b75a72432a9d891c9d2b28e8162dcdcb95d9e
Sean Breckenridge <seanbrecke@gmail.com>
2020-12-18 17:12:50 -0800

    bump version, remove emitted set

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with io.open("README.md", encoding="utf-8") as fo:
 
 setup(
     name="pushshift_comment_export",
-    version="0.1.0",
+    version="0.1.1",
     url="https://github.com/seanbreckenridge/pushshift_comment_export",
     author="Sean Breckenridge",
     author_email="seanbrecke@gmail.com",
