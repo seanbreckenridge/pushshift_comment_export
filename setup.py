852b9ef0f4d3e5f1f06d97e38959c2563dfbd4c1
Sean Breckenridge <seanbrecke@gmail.com>
2021-03-15 01:13:14 -0700

    add py.typed to package data

--- setup.py
+++ setup.py
@@ -7,9 +7,10 @@ requirements = ["logzero", "backoff", "requests", "click"]
 with io.open("README.md", encoding="utf-8") as fo:
     long_description = fo.read()
 
+pkg = "pushshift_comment_export"
 setup(
-    name="pushshift_comment_export",
-    version="0.1.1",
+    name=pkg,
+    version="0.1.2",
     url="https://github.com/seanbreckenridge/pushshift_comment_export",
     author="Sean Breckenridge",
     author_email="seanbrecke@gmail.com",
@@ -19,8 +20,9 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     license="MIT",
-    packages=find_packages(include=["pushshift_comment_export"]),
+    packages=find_packages(include=[pkg]),
     install_requires=requirements,
+    package_data={pkg: ["py.typed"]},
     keywords="reddit data",
     entry_points={
         "console_scripts": ["ps_comments = pushshift_comment_export.__main__:main"]
